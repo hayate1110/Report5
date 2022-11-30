4dc9680fad966ed7fa072e30832c751907993100
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2022-11-30 13:41:42 +0900

    Report5 step3

--- src/App.java
+++ src/App.java
@@ -1,11 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        }catch(NullPointerException e) {
-            System.out.println("NullPointerException発生");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
