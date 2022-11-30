af89ed534c8b80df36ce2db6eb221ff5c61103ea
Hayate Shiroma <e225719@ie.u-ryukyu.ac.jp>
2022-11-30 13:35:14 +0900

    Report5 step2

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        }catch(NullPointerException e) {
+            System.out.println("NullPointerException発生");
+            System.out.println(e.getMessage());
+        }
     }
 }
